6aed74205da22db4d7ef94f0cdd532a9b16b5dd3
BROADSTREETBULLY <hjw.personal@gmail.com>
2026-05-13 17:47:22 +1000

    Excercise 8 part 1 complete

--- Excercise2+3/Calculator/controllers/calculatorControllers.js
+++ Excercise2+3/Calculator/controllers/calculatorControllers.js
@@ -1,18 +1,22 @@
-const addNumbers = (req, res) => {
-  if (isNaN(parseInt(req.query.num1))) {
-    res.status(500).json({ error: `num1 is not a number` });
-    return;
-  }
+const Calculator = require("../libraries/Calculator");
+let calcObject = new Calculator();
 
-  if (isNaN(parseInt(req.query.num2))) {
-    res.status(500).json({ error: `num2 is not a number` });
-    return;
-  }
+
+const addNumbers = (req, res) => {
+  // if (isNaN(parseInt(req.query.num1))) {
+  //   res.status(500).json({ error: `num1 is not a number` });
+  //   return;
+  // }
+
+  // if (isNaN(parseInt(req.query.num2))) {
+  //   res.status(500).json({ error: `num2 is not a number` });
+  //   return;
+  // }
 
   let num1 = parseInt(req.query.num1);
   let num2 = parseInt(req.query.num2);
 
-  let result = num1 + num2;
+  let result = calcObject.add(num1, num2);
 
   res.json({
     data: result,
@@ -22,19 +26,19 @@ const addNumbers = (req, res) => {
 };
 
 const subtractNumbers = (req, res) => {
-  if (isNaN(parseInt(req.query.num1))) {
-    res.status(500).json({ error: `num1 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num1))) {
+  //   res.status(500).json({ error: `num1 is not a number` });
+  //   return;
+  // }
 
-  if (isNaN(parseInt(req.query.num2))) {
-    res.status(500).json({ error: `num2 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num2))) {
+  //   res.status(500).json({ error: `num2 is not a number` });
+  //   return;
+  // }
   let num1 = parseInt(req.query.num1);
   let num2 = parseInt(req.query.num2);
 
-  let result = num1 - num2;
+ let result = calcObject.subtract(num1, num2)
 
   res.json({
     data: result,
@@ -44,19 +48,19 @@ const subtractNumbers = (req, res) => {
 };
 
 const divideNumbers = (req, res) => {
-  if (isNaN(parseInt(req.query.num1))) {
-    res.status(500).json({ error: `num1 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num1))) {
+  //   res.status(500).json({ error: `num1 is not a number` });
+  //   return;
+  // }
 
-  if (isNaN(parseInt(req.query.num2))) {
-    res.status(500).json({ error: `num2 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num2))) {
+  //   res.status(500).json({ error: `num2 is not a number` });
+  //   return;
+  // }
   let num1 = parseInt(req.query.num1);
   let num2 = parseInt(req.query.num2);
 
-  let result = num1 / num2;
+  let result = calcObject.divide(num1, num2)
 
   res.json({
     data: result,
@@ -66,19 +70,19 @@ const divideNumbers = (req, res) => {
 };
 
 const multiplyNumbers = (req, res) => {
-  if (isNaN(parseInt(req.query.num1))) {
-    res.status(500).json({ error: `num1 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num1))) {
+  //   res.status(500).json({ error: `num1 is not a number` });
+  //   return;
+  // }
 
-  if (isNaN(parseInt(req.query.num2))) {
-    res.status(500).json({ error: `num2 is not a number` });
-    return;
-  }
+  // if (isNaN(parseInt(req.query.num2))) {
+  //   res.status(500).json({ error: `num2 is not a number` });
+  //   return;
+  // }
   let num1 = parseInt(req.query.num1);
   let num2 = parseInt(req.query.num2);
 
-  let result = num1 * num2;
+  let result = calcObject.multiply(num1, num2)
 
   res.json({
     data: result,
--- Excercise2+3/Calculator/index.js
+++ Excercise2+3/Calculator/index.js
@@ -1,9 +1,11 @@
-const app = require("./app")
+const app = require("./app");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const port = 3100
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+const port = 3100;
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+  console.log(`Example app listening at http://localhost:${port}`);
+});
